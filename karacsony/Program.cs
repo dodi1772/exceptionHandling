da4e6d571e623aec88ca37b9fe1b715fc638e8df
dodi1772 <zsigmond.dorian01@gmail.com>
2024-12-03 08:31:59 +0100

    Add project files.

---
+++ karacsony/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace karacsony
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Hi");
+            Console.WriteLine("help help help, gyerek");
+			int jogyerekek = 0;
+			bool siker = false;
+            while (!siker)
+            {
+                Console.WriteLine("pls jogyerekek szama: ");
+				string input=Console.ReadLine();
+				try
+				{
+					jogyerekek=Convert.ToInt32(input);
+                    if (jogyerekek<0)
+                    {
+                        throw new Exception("A jó gyerekek száma nem lehet negatív.");
+                    }
+                    siker = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Hiba: Csak számokat adhatsz meg.");
+                }
+                catch (Exception ex)
+				{
+                    Console.WriteLine($"Hiba: {ex.Message}");
+                }
+            }
+            Console.WriteLine($"thx miki: {jogyerekek}");
+        }
+	}
+}
